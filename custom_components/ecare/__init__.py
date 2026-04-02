c92215d6a86b9e69021bfd1aa1b9731814f3111b
Remko Weijnen <7575747+rweijnen@users.noreply.github.com>
2026-04-02 20:18:08 +0200

    fix: verwijder calendar uit PLATFORMS om integratie te herstellen (#12)

--- custom_components/ecare/__init__.py
+++ custom_components/ecare/__init__.py
@@ -22,7 +22,7 @@ from .const import (
 )
 
 _LOGGER = logging.getLogger(__name__)
-PLATFORMS = ["sensor", "calendar"]
+PLATFORMS = ["sensor"]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
